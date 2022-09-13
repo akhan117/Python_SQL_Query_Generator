e4a05dbd85614ea0ea6a1220c82a6b8aa203290b
akhan117 <ayaansuper@gmail.com>
2022-09-13 15:46:50 +0530

    Added talent and ascension level thresholds

--- main.py
+++ main.py
@@ -3,65 +3,120 @@ import querygenerate as qg
 import difflib as dl
 
 
-def select(cursor, table):
-    query = "select * from " + table
-    print(query)
-    cursor.execute(query)
-    row = cursor.fetchone()
+def select(cursor_m, tables):
+    for table in tables:
+        print()
+        print("This is " + table)
+        query = "select * from " + table
+        print(query)
+        cursor_m.execute(query)
+        row = cursor_m.fetchone()
 
-    try:
-        while row:
-            print(row)
-            row = cursor.fetchone()
-    except:
-        print("Yarghh")
+        try:
+            while row:
+                print(row)
+                row = cursor_m.fetchone()
+        except:
+            print("Yarghh")
 
-    save_query(query, "select " + table)
-
-
-def delete(cursor, matName):
-    cursor.execute("delete from Nmats where MatName = '" + matName + "'")
-    try:
-        cursor.fetchone()
-        row = cursor.fetchone()
-        while row:
-            print(row)
-            row = cursor.fetchone()
-    except:
-        print("No return statement")
+        save_query(query, "select " + table)
 
 
-def create_table(cursor):
-    c_info, ac_info = qg.dict_populate({}, {})
+def create_ascension_table(c_info, ac_info, t_name, cursor_m):
+    name = c_info["Name"].replace(" ", "_")
+
+    columns = "(m_name varchar(255), asc_20 int, asc_40 int, asc_50 int, asc_60 int, asc_70 int, asc_80 int)"
+
+    query = "create table " + t_name + " " + columns
+
+    cursor_m.execute(query)
+
+    for i in ac_info:
+        ac = ac_info[i]
+        query2 = "insert into " + t_name + " (m_name, asc_20, asc_40, asc_50, asc_60, asc_70, asc_80) " \
+                                           " values ('" + i + "', " + str(ac[0]) + ", " + str(ac[1]) + ", " \
+                 + str(ac[2]) + ", " + str(ac[3]) + ", " + str(ac[4]) + ", " + str(ac[5]) + ")"
+
+        cursor_m.execute(query2)
+        query = query + '\n' + query2
+
+    f_name = "create " + t_name
+    save_query(query, f_name)
+
+
+def create_talent_table(c_info, t_info, t_name, cursor_m):
+    name = c_info["Name"].replace(" ", "_")
+
+    columns = "(m_name varchar(255), t1 int, t2 int, t3 int, t4 int, t5 int, t6 int, t7 int, t8 int, t9 int, t10 int)"
+
+    query = "create table " + t_name + " " + columns
+
+    cursor_m.execute(query)
+
+    for i in t_info:
+        t = t_info[i]
+        query2 = "insert into " + t_name + " (m_name, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10) " \
+                                           " values ('" + i + "', " + str(t[0]) + ", " + str(t[1]) + ", " \
+                 + str(t[2]) + ", " + str(t[3]) + ", " + str(t[4]) + ", " + str(t[5]) + ", " + str(t[6]) + ", " \
+                 + str(t[7]) + ", " + str(t[8]) + ", " + str(t[9]) + ")"
+
+        cursor_m.execute(query2)
+        query = query + '\n' + query2
+
+    f_name = "create " + t_name
+    save_query(query, f_name)
+
+
+def create_tables(cursor_m):
+    c_info, ac_info, ac_cum, t_info, t_cum = qg.dict_populate({}, {}, {}, {}, {})
+
+    name = c_info["Name"].replace(" ", "_")
+    t1_name = name + "_Ascension_Materials"
+    create_ascension_table(c_info, ac_info, t1_name, cursor_m)
+
+    t2_name = name + "_Cumulative_Ascension_Materials"
+    create_ascension_table(c_info, ac_cum, t2_name, cursor_m)
+
+    t3_name = name + "_Talent_Materials"
+    create_talent_table(c_info, t_info, t3_name, cursor_m)
+
+    t4_name = name + "_Cumulative_Talent_Materials"
+    create_talent_table(c_info, t_cum, t4_name, cursor_m)
+
+
+def create_table(cursor_m):
+    c_info, ac_info, t_info, ac_cum, t_cum = qg.dict_populate({}, {}, {}, {}, {})
     qg.print_dict(c_info)
     qg.print_dict(ac_info)
+    qg.print_dict(t_info)
+
     name = c_info["Name"].replace(" ", "_")
     table_name = name + "_" + c_info["Nationality"] + "_" + c_info["Weapon"] + "_Materials"
     print(table_name)
-
     columns = "(Material_Name varchar(255), Number_Needed int, " \
               "Material_Type varchar(255), Material_Rarity int)"
 
     query = "create table " + table_name + " " + columns
 
     print(query)
-    cursor.execute(query)
+    cursor_m.execute(query)
     for i in ac_info:
         query2 = "insert into " + table_name + " (Material_Name, Number_Needed, Material_Type, Material_Rarity)" \
-                                               " values ('" + i + "'," + str(ac_info[i][0]) + ", '" + ac_info[i][1] +\
-                                                "', " + str(ac_info[i][2]) + ")"
-        cursor.execute(query2)
+                                               " values ('" + i + "'," + str(ac_info[i][0]) + ", '" + ac_info[i][1] + \
+                 "', " + str(ac_info[i][2]) + ")"
+        cursor_m.execute(query2)
         query = query + '\n' + query2
 
     f_name = "create " + table_name
     save_query(query, f_name)
 
 
-def drop_table(cursor, table):
-    query = "drop table " + table
-    print(query)
-    cursor.execute(query)
-    save_query(query, "drop " + table)
+def drop_table(cursor_m, tables):
+    for table in tables:
+        query = "drop table " + table
+        print(query)
+        cursor_m.execute(query)
+        save_query(query, "drop " + table)
 
 
 def table_choose():
@@ -114,6 +169,37 @@ def table_choose():
     return table
 
 
+def tables_choose():
+    print("Which Character? (Tables may take a while to show up.)")
+    cursor.execute("select * from sys.tables")
+    row = cursor.fetchone()
+    tables = []
+    tables_trunc = []
+    count = 1
+    while row:
+        r_name = (row[0].split('_'))[0]
+        tables_trunc.append(r_name)
+        tables.append(row[0])
+        row = cursor.fetchone()
+        count += 1
+
+    tables_trunc = list(set(tables_trunc))
+    qg.print_list(tables_trunc)
+    inp_m = input()
+    sel = qg.match_num_list(tables_trunc, int(inp_m))
+
+    cursor.execute("select * from sys.tables")
+    row = cursor.fetchone()
+    tables = []
+    while row:
+        r_name = (row[0].split('_'))[0]
+        if r_name == sel:
+            tables.append(row[0])
+        row = cursor.fetchone()
+
+    return tables
+
+
 def save_query(query, f_name):
     with open("Queries/" + f_name + ".sql", "w+") as f:
         f.write("use " + database + '\n')
@@ -126,21 +212,23 @@ if __name__ == '__main__':
                       password=r'2022', database=database)
     cursor = conn.cursor()
 
-    inp = input("1. Create, 2. Drop, 3. Select :")
+    inp = input("1. Create a Character Entry, 2. Drop, 3. Select :")
 
     if inp == '1':
-        create_table(cursor)
+        create_tables(cursor)
+
     elif inp == '2':
-        table_name = table_choose()
-        print("Selected: " + table_name)
-        confirm = input("Are you sure you want to delete this table?(y/n): ")
+        table_names = tables_choose()
+        qg.print_list(table_names)
+        confirm = input("Are you sure you want to delete these table(s)?(y/n): ")
         if confirm == 'y':
-            drop_table(cursor, table_name)
+            drop_table(cursor, table_names)
 
     elif inp == '3':
-        table_name = table_choose()
-        print("Selected: " + table_name)
-        select(cursor, table_name)
+        table_names = tables_choose()
+        print("Selected: ")
+        qg.print_list(table_names)
+        select(cursor, table_names)
 
     conn.commit()
     conn.close()
--- querygenerate.py
+++ querygenerate.py
@@ -1,3 +1,4 @@
+import copy
 import difflib as dl
 
 weapons = ["sword", "claymore", "polearm", "bow", "catalyst"]
@@ -65,6 +66,26 @@ common_material_relations = {'Slime Condensate': ['Slime Condensate', 'Slime Sec
                              'Fungal Spores': ['Fungal Spores', 'Luminescent Pollen', 'Crystalline Cyst Dust'],
                              'Fungal Red Satin': ['Fungal Red Satin', 'Trimmed Red Silk', 'Rich Red Brocade'],
                              }
+
+# Book 1, Book 2, Book 3, Enemy 1, Enemy 2, Enemy 3, Weekly, Mora, Crown
+talent_levels = {1: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 2: [3, 0, 0, 6, 0, 0, 0, 12500, 0],
+                 3: [0, 2, 0, 0, 3, 0, 0, 17500, 0],
+                 4: [0, 4, 0, 0, 4, 0, 0, 25000, 0],
+                 5: [0, 6, 0, 0, 6, 0, 0, 30000, 0],
+                 6: [0, 9, 0, 0, 9, 0, 0, 37500, 0],
+                 7: [0, 0, 4, 0, 0, 4, 1, 120000, 0],
+                 8: [0, 0, 6, 0, 0, 6, 1, 260000, 0],
+                 9: [0, 0, 12, 0, 0, 9, 2, 450000, 0],
+                 10: [0, 0, 16, 0, 0, 12, 2, 700000, 1]}
+
+# sliver, shard, chunk, gem, enemy 1, enemy 2, enemy 3, local, boss, Mora]
+ascension_levels = {20: [1, 0, 0, 0, 3, 0, 0, 3, 0, 20000 + 24200],
+                    40: [0, 3, 0, 0, 15, 0, 0, 10, 2, 40000 + 115800],
+                    50: [0, 6, 0, 0, 0, 12, 0, 20, 4, 60000 + 116000],
+                    60: [0, 0, 3, 0, 0, 18, 0, 30, 8, 80000 + 171000],
+                    70: [0, 0, 6, 0, 0, 0, 12, 45, 12, 100000 + 239200],
+                    80: [0, 0, 0, 6, 0, 0, 24, 60, 20, 120000 + 322400]}
 
 
 def insert_name(c_info):
@@ -208,43 +229,99 @@ def close_insert(c_info, inp, key):
         raise Exception("Invalid " + key + "!'")
 
 
-def level_90_ascension(c_info, ac_info):
+def dict_append(appender, appendee):
+    if appender:
+        appender.append(appendee)
+    else:
+        appender = [appendee]
+
+
+def ascension(c_info, ac_info, acl, a_suc):
     if c_info["Element"] != "Unknown":
         gem_type = gem_relations[c_info["Element"]]
-        ac_info[gem_type + " Sliver"] = (1, "Elemental Jewel", 2)
-        ac_info[gem_type + " Fragment"] = (9, "Elemental Jewel", 3)
-        ac_info[gem_type + " Chunk"] = (9, "Elemental Jewel", 4)
-        ac_info[gem_type + " Gemstone"] = (6, "Elemental Jewel", 5)
-
-    if c_info["Talent Materials"] != "Unknown":
-        talent_type = c_info["Talent Materials"]
-        ac_info["Teachings of " + talent_type] = (9, "Talent Book", 2)
-        ac_info["Guide to " + talent_type] = (63, "Talent Book", 3)
-        ac_info["Philosophies of " + talent_type] = (114, "Talent Book", 4)
+        ac_info.setdefault(gem_type + " Sliver", []).append(a_suc[acl][0])
+        ac_info.setdefault(gem_type + " Fragment", []).append(a_suc[acl][1])
+        ac_info.setdefault(gem_type + " Chunk", []).append(a_suc[acl][2])
+        ac_info.setdefault(gem_type + " Gemstone", []).append(a_suc[acl][3])
 
     if c_info["Common Materials"] != "Unknown":
         com_mat = common_material_relations[c_info["Common Materials"]]
-        com_mat_num = [36, 96, 129]
         for n, i in enumerate(com_mat):
-            ac_info[i] = (com_mat_num[n], "Enemy Drop",  n + 1)
+            ac_info.setdefault(i, []).append(a_suc[acl][4 + n])
 
     if c_info["Local Materials"] != "Unknown":
-        ac_info[c_info["Local Materials"]] = (168, "Local Speciality", 1)
+        ac_info.setdefault(c_info["Local Materials"], []).append(a_suc[acl][7])
 
     if c_info["World Boss Materials"] != "Unknown":
-        ac_info[c_info["World Boss Materials"]] = (46, "World Boss Material", 4)
+        ac_info.setdefault(c_info["World Boss Materials"], []).append(a_suc[acl][8])
+
+    ac_info.setdefault("Mora", []).append(a_suc[acl][9])
+
+
+def talents(c_info, t_info, tl, t_suc):
+    if c_info["Talent Materials"] != "Unknown":
+        talent_type = c_info["Talent Materials"]
+        t_info.setdefault("Teachings of " + talent_type, []).append(t_suc[tl][0])
+        t_info.setdefault("Guide to " + talent_type, []).append(t_suc[tl][1])
+        t_info.setdefault("Philosophies of " + talent_type, []).append(t_suc[tl][2])
+
+    if c_info["Common Materials"] != "Unknown":
+        com_mat = common_material_relations[c_info["Common Materials"]]
+        for n, i in enumerate(com_mat):
+            t_info.setdefault(i, []).append(t_suc[tl][3 + n])
 
     if c_info["Weekly Boss Materials"] != "Unknown":
-        ac_info[c_info["Weekly Boss Materials"]] = (18, "Weekly Boss Drop", 5)
+        t_info.setdefault(c_info["Weekly Boss Materials"], []).append(t_suc[tl][6])
 
-    ac_info["Mora"] = (1673400 + (3 * 1652500) + 420000, "Mora", 3)
-    ac_info["Crown of Insight"] = (3, "Crown", 5)
+    t_info.setdefault("Mora", []).append(t_suc[tl][7])
+    t_info.setdefault("Crowns", []).append(t_suc[tl][8])
+
+
+# def level_90_ascension(c_info, ac_info, t_info):
+#     a_cum = ascension_addition()
+#     t_cum = talent_addition()
+#     a_suc = ascension_levels
+#     c_suc = talent_levels
+#
+#     if c_info["Element"] != "Unknown":
+#         gem_type = gem_relations[c_info["Element"]]
+#         ac_info[gem_type + " Sliver"] = 1
+#         ac_info[gem_type + " Fragment"] = 9
+#         ac_info[gem_type + " Chunk"] = 9
+#         ac_info[gem_type + " Gemstone"] = 6
+#
+#     if c_info["Talent Materials"] != "Unknown":
+#         talent_type = c_info["Talent Materials"]
+#         t_info["Teachings of " + talent_type] = 9
+#         t_info["Guide to " + talent_type] = 63
+#         t_info["Philosophies of " + talent_type] = 114
+#
+#     if c_info["Common Materials"] != "Unknown":
+#         com_mat = common_material_relations[c_info["Common Materials"]]
+#         com_mat_num_asc = [18, 30, 36]
+#         com_mat_num_tal = [6 * 3, 22 * 3, 31 * 3]
+#         for n, i in enumerate(com_mat):
+#             ac_info[i] = com_mat_num_asc[n]
+#             t_info[i] = com_mat_num_tal[n]
+#
+#     if c_info["Local Materials"] != "Unknown":
+#         ac_info[c_info["Local Materials"]] = 168
+#
+#     if c_info["World Boss Materials"] != "Unknown":
+#         ac_info[c_info["World Boss Materials"]] = 46
+#
+#     if c_info["Weekly Boss Materials"] != "Unknown":
+#         t_info[c_info["Weekly Boss Materials"]] = 18
+#
+#     ac_info["Mora"] = 1673400 + 420000
+#     t_info["Mora"] = 3 * 1652500
+#     t_info["Crown of Insight"] = 3
 
 
 def print_dict(char_dict):
     print()
     for var in char_dict:
-        print(var + ": " + str(char_dict[var]))
+        print(str(var) + ": " + str(char_dict[var]))
 
     print()
 
@@ -265,8 +342,30 @@ def match_num_list(mat_list, num):
     raise Exception("Number was not between the confines of 1 and " + str(i))
 
 
-def dict_populate(c_info, ac_info):
+def talent_addition():
+    talent_cumulative = {}
+    talent_sum = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for i in range(1, 11):
+        for j in range(0, 9):
+            talent_sum[j] = talent_sum[j] + talent_levels[i][j]
 
+        talent_cumulative[i] = copy.copy(talent_sum)
+    return talent_cumulative
+
+
+def ascension_addition():
+    ascension_cumulative = {}
+    ascension_sum = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ascensions = [20, 40, 50, 60, 70, 80]
+    for i in ascensions:
+        for j in range(0, 10):
+            ascension_sum[j] = ascension_sum[j] + ascension_levels[i][j]
+
+        ascension_cumulative[i] = copy.copy(ascension_sum)
+    return ascension_cumulative
+
+
+def dict_populate(c_info, ac_info, t_info, ac_info_cum, t_info_cum):
     insert_name(c_info)
     insert_weapon(c_info)
     insert_element(c_info)
@@ -276,17 +375,39 @@ def dict_populate(c_info, ac_info):
     insert_common_mat(c_info)
     insert_world_boss_drop(c_info)
     insert_weekly_boss_drop(c_info)
-    level_90_ascension(c_info, ac_info)
+
+    asc = [20, 40, 50, 60, 70, 80]
+    for i in asc:
+        ascension(c_info, ac_info, i, ascension_levels)
+        ascension(c_info, ac_info_cum, i, ascension_addition())
+
+    for i in range(1, 11):
+        talents(c_info, t_info, i, talent_levels)
+        talents(c_info, t_info_cum, i, talent_addition())
+
+    # level_90_ascension(c_info, ac_info, t_info)
     print_dict(c_info)
+
+    print("Ascension Information")
     print_dict(ac_info)
+    print("Cumulative Ascension Information")
+    print_dict(ac_info_cum)
+    print("Talent Information")
+    print_dict(t_info)
+    print("Cumulative Talent Information")
+    print_dict(t_info_cum)
 
     inp = input("Are these correct? (y/n)")
     if inp != 'y':
         raise Exception("Then do it again")
-    return [c_info, ac_info]
+    return [c_info, ac_info, ac_info_cum, t_info, t_info_cum]
 
 
 if __name__ == '__main__':
-    c1_info = {}
-    ac1_info = {}
-    dict_populate(c1_info, ac1_info)
+    character_info = {}
+    ascension_info = {}
+    talent_info = {}
+    ascension_info_cum = {}
+    talent_info_cum = {}
+
+    dict_populate(character_info, ascension_info, talent_info, ascension_info_cum, talent_info_cum)
